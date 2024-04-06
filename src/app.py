5bcb841989a5eb4e44c612a7d26d15152352c5ec
Sivakorn Chong <sivakornchong@gmail.com>
2024-04-06 02:58:15 -0700

    test5

--- src/app.py
+++ src/app.py
@@ -14,7 +14,7 @@ import altair as alt
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-df = pd.read_csv('data/raw/van_weather_1974-01-01_2024-03-15.csv', encoding='latin-1', index_col='date', parse_dates=True)
+df = pd.read_csv('../data/raw/van_weather_1974-01-01_2024-03-15.csv', encoding='latin-1', index_col='date', parse_dates=True)
 df['date'] = pd.to_datetime(df.index)
 df['year'] = df['date'].dt.year
 
